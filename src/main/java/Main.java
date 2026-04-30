027ac332d2806a87eb49556cd8b1d3b1b3563c2f
m-reinisch <michael.reinisch@email.de>
2026-04-30 15:48:00 +0200

    GREEN isValid("Abcdef1g")

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,24 @@ public class Main {
         System.out.println("Hello World!");
     }
 
+    /** Checks the password for validity.
+     *
+     * @param password to check
+     * @return true, if valid, else false
+     */
     public static boolean isValid(String password){
+        boolean test1= false;
+        boolean test2= false;
+        boolean test3= false;
+        boolean test4= false;
+
+        test1= PasswordValidator.hasMinLength(password, 8);
+        test2= PasswordValidator.containsDigit(password);
+        test3= PasswordValidator.containsUpperAndLower(password);
+        test4= PasswordValidator.isCommonPassword(password);
+        if (test1 && test2 && test3 && (!test4)){
+            return true;
+        }
         return false;
     }
 }
