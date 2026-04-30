e28ee4315862dfe6debe88bfa3f2a07e7ada0084
m-reinisch <michael.reinisch@email.de>
2026-04-30 12:37:21 +0200

    GREEN of containsDigit("012345678")

--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -89,6 +89,16 @@ class PasswordValidatorTest {
     }
 
     @Test
+    void containsDigit_shouldBeTrue_whenContainsOnlyDigits() {
+        String password= "012345678";
+        boolean expected= true;
+        boolean actual;
+
+        actual= PasswordValidator.containsDigit(password);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void containsUpperAndLower() {
     }
 
